1314c9fb6510aa96831ae0a70465014cb3c27e05
Wesley Pettit <wppttt@amazon.com>
2019-12-10 23:38:15 -0800

    WIP: set of standard credentials providers

---
+++ src/flb_aws_credentials.c
@@ -0,0 +1,153 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*  Fluent Bit
+ *  ==========
+ *  Copyright (C) 2019      The Fluent Bit Authors
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#include <fluent-bit/flb_info.h>
+#include <fluent-bit/flb_sds.h>
+#include <fluent-bit/flb_http_client.h>
+#include <fluent-bit/flb_aws_credentials.h>
+
+#include <stdlib.h>
+
+/*
+ * A provider that wraps other providers and adds a cache.
+ */
+static struct aws_credentials_provider_cached {
+    struct aws_credentials *credentials;
+    unsigned long next_refresh;
+    unsigned long ttl;
+
+    /* Underlying provider */
+    struct aws_credentials_provider *provider;
+};
+
+/*
+ * A provider that obtains credentials from an http endpoint.
+ * On ECS the ECS Agent vends credentials via a link local IP address.
+ * Some customers build local HTTP services that provide the same functionality.
+ */
+static struct aws_credentials_provider_http {
+    struct aws_credentials *credentials;
+    unsigned long expiration;
+
+    /* upstream connection to host */
+    struct flb_upstream *upstream;
+
+    /* Host and Path to request credentials */
+    char *host;
+    char *path;
+ };
+
+/*
+ * A provider that obtains credentials from EC2 IMDS.
+ */
+static struct aws_credentials_provider_imds {
+    struct aws_credentials *credentials;
+    unsigned long expiration;
+
+    /* upstream connection to IMDS */
+    struct flb_upstream *upstream;
+ };
+
+/*
+ * The standard credential provider chain:
+ * 1. Environment variables
+ * 2. Shared credentials file (AWS Profile)
+ * 3. EC2 IMDS
+ * 4. ECS HTTP credentials endpoint
+ *
+ * This provider will evaluate each provider in order, returning the result
+ * from the first provider that returns valid credentials.
+ *
+ * Note: Client code should use this provider by default.
+ */
+struct aws_credentials_provider_default_chain {
+    struct aws_credentials_provider *env_provider;
+    struct aws_credentials_provider *profile_provider;
+    struct aws_credentials_provider *ec2_provider;
+    struct aws_credentials_provider *ecs_provider;
+};
+
+
+aws_credentials *get_credentials_fn_environment(struct aws_credentials_provider *provider) {
+    char *access_key;
+    char *secret_key;
+    char *session_token;
+    aws_credentials *creds;
+
+    access_key = getenv(AWS_ACCESS_KEY_ID);
+    if (!access_key) {
+        return NULL
+    }
+
+    secret_key = getenv(AWS_SECRET_ACCESS_KEY);
+    if (!secret_key) {
+        flb_free(access_key);
+        return NULL
+    }
+
+    creds = flb_malloc(sizeof(struct aws_credentials));
+    if (!creds) {
+        flb_free(access_key);
+        flb_free(secret_key);
+        return NULL
+    }
+
+    creds->access_key_id = flb_sds_create(access_key);
+    creds->secret_access_key = flb_sds_create(secret_key);
+
+    session_token = getenv(AWS_SESSION_TOKEN);
+    if (session_token) {
+        creds->session_token = session_token;
+    }
+
+    return creds;
+
+}
+
+/* Refresh is a no-op for the env provider */
+int refresh_fn_environment(struct aws_credentials_provider *provider) {
+    return 0;
+}
+
+
+/* Destroy is a no-op for the env provider */
+void destroy_fn_environment(struct aws_credentials_provider *provider) {
+    return;
+}
+
+static struct aws_credentials_provider_vtable environment_provider_vtable = {
+    .get_credentials = get_credentials_fn_environment,
+    .refresh = refresh_fn_environment,
+    .destroy = destroy_fn_environment,
+};
+
+struct aws_credentials_provider *new_environment_provider() {
+    struct aws_credentials_provider provider = flb_malloc(
+                                                          sizeof(
+                                                          struct aws_credentials_provider));
+
+    if (!provider) {
+        return NULL;
+    }
+
+    provider->provider_vtable = &environment_provider_vtable;
+    provider->implementation = NULL;
+
+    return provider;
+}
---
+++ src/flb_aws_credentials.h
@@ -0,0 +1,129 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*  Fluent Bit
+ *  ==========
+ *  Copyright (C) 2019      The Fluent Bit Authors
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#ifdef FLB_HAVE_AWS_CREDS
+
+#ifndef FLB_AWS_CREDENTIALS_H
+#define FLB_AWS_CREDENTIALS_H
+
+
+/* Credentials Environment Variables */
+#define AWS_ACCESS_KEY_ID          "AWS_ACCESS_KEY_ID"
+#define AWS_SECRET_ACCESS_KEY      "AWS_SECRET_ACCESS_KEY"
+#define AWS_SESSION_TOKEN          "AWS_SESSION_TOKEN"
+
+
+
+/*
+ * A structure that wraps the sensitive data needed to sign an AWS request
+ */
+struct aws_credentials {
+    flb_sds_t access_key_id;
+    flb_sds_t secret_access_key;
+    flb_sds_t session_token;
+};
+
+/*
+ * Function to free memory used by an aws_credentials structure
+ */
+void aws_credentials_destroy(struct aws_credentials *creds);
+
+/*
+ * Get credentials using the provider.
+ * Client is in charge of freeing the returned credentials struct.
+ * Returns NULL if credentials could not be obtained.
+ */
+typedef aws_credentials*(aws_credentials_provider_get_credentials_fn)(struct aws_credentials_provider *provider);
+
+/*
+ * Force a refesh of credentials. This is needed for providers that cache
+ * credentials. If the client receives a response from AWS indicating that
+ * the credentials are expired, they can call this method.
+ */
+typedef int(aws_credentials_provider_refresh_fn)(struct aws_credentials_provider *provider);
+
+
+/*
+ * Clean up the underlying provider implementation
+ * Clients should call this and then free the aws_credentials_provider structure.
+ */
+typedef void(aws_credentials_provider_destroy_fn)(struct aws_credentials_provider *provider);
+
+/*
+ * This structure is a virtual table that allows the client to get credentials.
+ * And clean up all memory from the underlying implementation.
+ */
+struct aws_credentials_provider_vtable {
+    aws_credentials_provider_get_credentials_fn *get_credentials;
+    aws_credentials_provider_refresh_fn *refresh;
+    aws_credentials_provider_destroy_fn *destroy;
+};
+
+/*
+ * A generic structure to represent all providers.
+ */
+struct aws_credentials_provider {
+    struct aws_credentials_provider_vtable *provider_vtable;
+    void *implementation;
+};
+
+/*
+ * A provider that wraps another provider and adds a cache.
+ */
+struct aws_credentials_provider *new_cached_provider(struct
+                                                    aws_credentials_provider
+                                                    *provider, unsigned long ttl);
+
+/*
+ * Standard Environment variables
+ */
+struct aws_credentials_provider *new_environment_provider();
+
+/*
+ * New EC2 IMDS provider
+ */
+struct aws_credentials_provider *new_imds_provider();
+
+/*
+ * New ECS provider
+ */
+struct aws_credentials_provider *new_ecs_provider();
+
+/*
+ * New http provider
+ */
+struct aws_credentials_provider *new_http_provider();
+
+/*
+ * The standard credential provider chain:
+ * 1. Environment variables
+ * 2. Shared credentials file (AWS Profile)
+ * 3. EC2 IMDS
+ * 4. ECS HTTP credentials endpoint
+ *
+ * This provider will evaluate each provider in order, returning the result
+ * from the first provider that returns valid credentials.
+ *
+ * Note: Client code should use this provider by default.
+ */
+struct aws_credentials_provider *new_standard_chain_provider();
+
+
+#endif
+#endif /* FLB_HAVE_AWS_CREDS */
