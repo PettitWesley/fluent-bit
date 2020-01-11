bc47fcec4e39a6be4a88a0368b44b1c4324ee968
Wesley Pettit <wppttt@amazon.com>
2020-01-11 11:45:25 -0800

    wip

--- src/flb_signv4.c
+++ src/flb_signv4.c
@@ -632,8 +632,8 @@ static flb_sds_t flb_signv4_canonical_request(struct flb_http_client *c,
 
     /* host must always be included */
     host = flb_sds_create_size(256);
-    tmp = flb_sds_printf(&host, "%s:%i", c->u_conn->tcp_host,
-                         c->u_conn->tcp_port);
+    tmp = flb_sds_printf(&host, "%s:%i", c->u_conn->u->tcp_host,
+                         c->u_conn->u->tcp_port);
     if (!tmp) {
         flb_errno();
         flb_kv_release(&list_tmp);
