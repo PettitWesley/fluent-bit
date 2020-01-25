58816eb8bbf8f0fb2697f9fc97e64a7bf30cb823
Wesley Pettit <wppttt@amazon.com>
2020-01-25 07:40:12 -0800

    wip

--- src/flb_signv4.c
+++ src/flb_signv4.c
@@ -767,7 +767,10 @@ static flb_sds_t flb_signv4_canonical_request(struct flb_http_client *c,
     /* Signed Headers for authorization header (Task 4) */
     for (i = 0; i < items; i++) {
         kv = (struct flb_kv *) arr[i];
-
+        if (strcasecmp(kv->key, "content-length") == 0) {
+            flb_debug("Skipping non-signed header %s", kv->key);
+            continue;
+        }
         /* Check if this is the last header, if so add breakline separator */
         if (i + 1 == items) {
             tmp = flb_sds_printf(signed_headers, "%s", kv->key);
