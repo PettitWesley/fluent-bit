592f5a38a75aab00807542ad6e356bd860081b3b
Wesley Pettit <wppttt@amazon.com>
2020-01-30 00:26:32 -0800

    wip

--- src/flb_http_client.c
+++ src/flb_http_client.c
@@ -326,8 +326,8 @@ static int process_data(struct flb_http_client *c)
     if (c->resp.status <= 0) {
         memcpy(code, c->resp.data + 9, 3);
         code[3] = '\0';
-        flb_info("this should be the http code: %s", code);
         c->resp.status = atoi(code);
+        flb_info("this should be the http code: %s and it is: %d", code, c->resp.status);
     }
 
     /* Try to lookup content length */
