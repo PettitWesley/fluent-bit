b7fe30595e570d08fbc8da8eb67cb095aafd19d6
Wesley Pettit <wppttt@amazon.com>
2020-02-29 12:38:01 -0800

    wip

--- plugins/out_stdout/stdout.c
+++ plugins/out_stdout/stdout.c
@@ -279,15 +279,7 @@ static int compare_events(const void *a_arg, const void *b_arg)
     struct event *r_a = *(struct event **) a_arg;
     struct event *r_b = *(struct event **) b_arg;
 
-    if (r_a->timestamp == r_b->timestamp) {
-        return 0;
-    }
-    else if (r_a->timestamp < r_b->timestamp) {
-        return -1;
-    }
-    else {
-        return 1;
-    }
+    return r_a->timestamp < r_b->timestamp);
 }
 
 static void cb_stdout_flush(const void *data, size_t bytes,
