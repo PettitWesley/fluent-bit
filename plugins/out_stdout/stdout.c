15edc0980ee899f0f3d0c56fb20c4bf8be32cb10
Wesley Pettit <wppttt@amazon.com>
2020-03-07 14:32:25 -0800

    wip

--- plugins/out_stdout/stdout.c
+++ plugins/out_stdout/stdout.c
@@ -87,7 +87,7 @@ static int cb_stdout_init(struct flb_output_instance *ins,
     //     return -1;
     // }
 
-    ctx->provider = flb_standard_chain_provider_create(config, NULL, "us-west-2", NULL
+    ctx->provider = flb_standard_chain_provider_create(config, NULL, "us-west-2", NULL,
                                                        flb_aws_client_generator());
     if (!ctx->provider) {
         flb_errno();
