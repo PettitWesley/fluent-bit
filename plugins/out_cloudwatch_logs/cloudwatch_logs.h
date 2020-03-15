839c59e2f7118946266cb73d15dcfef3ce3f46e8
Wesley Pettit <wppttt@amazon.com>
2020-03-15 00:01:17 -0700

    wip

--- plugins/out_cloudwatch_logs/cloudwatch_logs.h
+++ plugins/out_cloudwatch_logs/cloudwatch_logs.h
@@ -93,7 +93,7 @@ struct flb_cloudwatch {
     char *endpoint;
 
     struct event *events;
-    size_t events_size;
+    int events_len;
 
     /* if we're writing to a static log stream, we'll use this */
     struct log_stream stream;
