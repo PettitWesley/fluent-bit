2237eaeafa776d87cb5315767b6b0ad5dc22307f
Wesley Pettit <wppttt@amazon.com>
2020-04-12 20:10:46 -0700

    wip

--- plugins/out_cloudwatch_logs/cloudwatch_api.c
+++ plugins/out_cloudwatch_logs/cloudwatch_api.c
@@ -402,6 +402,7 @@ retry:
 
     for (i = 0; i < buf->event_index; i++) {
         event = &buf->events[i];
+        printf("debug: i: %d", i);
         ret = write_event(ctx, buf, event, &offset);
         if (ret < 0) {
             flb_plg_error(ctx->ins, "Failed to write log event %d to "
