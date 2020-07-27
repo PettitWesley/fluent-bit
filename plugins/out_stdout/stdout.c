714c7bee9cf73f50e28beaa013efcfc9e629c334
Wesley Pettit <wppttt@amazon.com>
2020-07-26 18:00:19 -0700

    wip

--- plugins/out_stdout/stdout.c
+++ plugins/out_stdout/stdout.c
@@ -412,7 +412,7 @@ static void cb_stdout_flush(const void *data, size_t bytes,
         FLB_OUTPUT_RETURN(FLB_OK);
     }
 
-    ret = construct_request_buffer(ctx, json, chunk, &buffer, buffer_size);
+    ret = construct_request_buffer(ctx, json, chunk, &buffer, &buffer_size);
     if (ret < 0) {
         flb_plg_error(ctx->ins, "Could not construct request buffer %s",
                       chunk->file_path);
