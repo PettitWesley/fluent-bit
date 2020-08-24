47b50a2e88a95f8c2ed919ce097ef415d228d2cb
Wesley Pettit <wppttt@amazon.com>
2020-08-23 18:46:04 -0700

    wip

--- plugins/out_kinesis_firehose/firehose_api.c
+++ plugins/out_kinesis_firehose/firehose_api.c
@@ -60,6 +60,7 @@ static inline int try_to_write(char *buf, int *off, size_t left,
         str_len = strlen(str);
     }
     if (left <= *off+str_len) {
+        flb_info("%d <= %d + %d", left, *off, str_len);
         return FLB_FALSE;
     }
     memcpy(buf+*off, str, str_len);
