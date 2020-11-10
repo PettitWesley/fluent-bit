57381225fb9a9fed3172db12248aeaef2b67d2a8
Wesley Pettit <wppttt@amazon.com>
2020-11-09 17:27:16 -0800

    http: Show how to print the headers for debugging
    Signed-off-by: Wesley Pettit <wppttt@amazon.com>

--- src/flb_http_client.c
+++ src/flb_http_client.c
@@ -1066,6 +1066,8 @@ int flb_http_do(struct flb_http_client *c, size_t *bytes)
     }
 #endif
 
+    flb_info("%.*s",  c->header_len, c->header_buf);
+
     /* Write the header */
     ret = flb_io_net_write(c->u_conn,
                            c->header_buf, c->header_len,
