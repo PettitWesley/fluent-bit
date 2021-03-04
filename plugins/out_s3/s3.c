dfaa8ff2c48f0716e9590083548249cfa69cd1b5
Wesley Pettit <wppttt@amazon.com>
2021-03-03 16:15:30 -0800

    wip

--- plugins/out_s3/s3.c
+++ plugins/out_s3/s3.c
@@ -710,7 +710,9 @@ static int cb_s3_init(struct flb_output_instance *ins,
     }
 
     /* this is done last since in the previous block we make calls to AWS */
+    ctx->ins->use_tls = FLB_FALSE;
     ctx->provider->provider_vtable->upstream_set(ctx->provider, ctx->ins);
+    ctx->ins->use_tls = FLB_TRUE;
 
     return 0;
 }
