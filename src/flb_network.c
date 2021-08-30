423d6e954b6a52441805413bd2e87944acba7329
Wesley Pettit <wppttt@amazon.com>
2021-08-30 10:44:32 -0700

    Add log message for DNS mode
    Signed-off-by: Wesley Pettit <wppttt@amazon.com>

--- src/flb_network.c
+++ src/flb_network.c
@@ -832,7 +832,10 @@ static struct flb_dns_lookup_context *flb_net_dns_lookup_context_create(
     opts.tries = 2;
 
     if (dns_mode == FLB_DNS_USE_TCP) {
+        flb_warn("Forcing TCP for DNS");
         opts.flags = ARES_FLAG_USEVC;
+    } else {
+        flb_info("Allowing UDP for DNS");
     }
 
     *result = ares_init_options((ares_channel *) &lookup_context->ares_channel,
