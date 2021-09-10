b7964d2bd2a2f047b39f7a519fb662b1cad6fd9d
Wesley Pettit <wppttt@amazon.com>
2021-09-09 17:22:26 -0700

    out_cloudwatch_logs: auto retry invalid requests
    Signed-off-by: Wesley Pettit <wppttt@amazon.com>

--- plugins/out_cloudwatch_logs/cloudwatch_api.c
+++ plugins/out_cloudwatch_logs/cloudwatch_api.c
@@ -1298,6 +1298,7 @@ int put_log_events(struct flb_cloudwatch *ctx, struct cw_flush *buf,
     flb_sds_t tmp;
     flb_sds_t error;
     int num_headers = 1;
+    int retry = FLB_TRUE;
 
     buf->put_events_calls++;
 
@@ -1321,6 +1322,7 @@ int put_log_events(struct flb_cloudwatch *ctx, struct cw_flush *buf,
         num_headers = 2;
     }
 
+retry_request:
     if (plugin_under_test() == FLB_TRUE) {
         c = mock_http_call("TEST_PUT_LOG_EVENTS_ERROR", "PutLogEvents");
     }
@@ -1357,13 +1359,25 @@ int put_log_events(struct flb_cloudwatch *ctx, struct cw_flush *buf,
         
             if (c->resp.data == NULL || c->resp.data_len == 0 || strstr(c->resp.data, AMZN_REQUEST_ID_HEADER) == NULL) {
                 /* code was 200, but response is invalid, treat as failure */
-                flb_plg_error(ctx->ins, "Recieved code 200 but response was invalid, %s header not found",
-                              AMZN_REQUEST_ID_HEADER);
+                if (retry == FLB_TRUE) {
+                    flb_plg_debug(ctx->ins, "Recieved code 200 but response was invalid, %s header not found",
+                                  AMZN_REQUEST_ID_HEADER);
+                }
+                else {
+                    flb_plg_error(ctx->ins, "Recieved code 200 but response was invalid, %s header not found",
+                                  AMZN_REQUEST_ID_HEADER);
+                }
                 if (c->resp.data != NULL) {
                     flb_plg_debug(ctx->ins, "Could not find sequence token in "
                                   "response: response body is empty: full data: `%.*s`", c->resp.data_len, c->resp.data);
                 }
                 flb_http_client_destroy(c);
+
+                if (retry == FLB_TRUE) {
+                    flb_plg_debug(ctx->ins, "issuing immediate retry for invalid request");
+                    retry = FLB_FALSE;
+                    goto retry_request;
+                }
                 return -1;
             }
             
