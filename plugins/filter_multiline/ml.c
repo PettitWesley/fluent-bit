c2d44fe6e1966b52e5303f6fccbb19a7fdcb1582
Wesley Pettit <wppttt@amazon.com>
2021-10-10 14:43:43 -0700

    wip

--- plugins/filter_multiline/ml.c
+++ plugins/filter_multiline/ml.c
@@ -181,7 +181,7 @@ static int cb_ml_filter(const void *data, size_t bytes,
     msgpack_unpacked_init(&result);
     while (msgpack_unpack_next(&result, data, bytes, &off) == ok) {
 
-        json = flb_msgpack_to_json_str(1000, obj)
+        json = flb_msgpack_to_json_str(1000, obj);
         if (json != NULL) {
             flb_plg_info(ctx->ins, "incoming record: `%s`", json);
             needle = strchr(json, '\n');
