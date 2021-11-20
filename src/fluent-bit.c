06c3a6c4679819f3123922b30bc7924490edfb0c
Wesley Pettit <wppttt@amazon.com>
2021-11-20 15:24:30 -0800

    wip

--- src/fluent-bit.c
+++ src/fluent-bit.c
@@ -569,15 +569,15 @@ static void flb_signal_handler(int signal)
 
 static void flb_signal_init()
 {
-    signal(SIGINT,  &flb_signal_handler_break_loop);
-#ifndef FLB_SYSTEM_WINDOWS
-    signal(SIGQUIT, &flb_signal_handler_break_loop);
-    signal(SIGHUP,  &flb_signal_handler_break_loop);
-    signal(SIGCONT, &flb_signal_handler);
-#endif
-    //signal(SIGTERM, &flb_signal_handler_break_loop);
-    signal(SIGSEGV, &flb_signal_handler);
-    signal(SIGFPE,  &flb_signal_handler);
+//     signal(SIGINT,  &flb_signal_handler_break_loop);
+// #ifndef FLB_SYSTEM_WINDOWS
+//     signal(SIGQUIT, &flb_signal_handler_break_loop);
+//     signal(SIGHUP,  &flb_signal_handler_break_loop);
+//     signal(SIGCONT, &flb_signal_handler);
+// #endif
+//     //signal(SIGTERM, &flb_signal_handler_break_loop);
+//     signal(SIGSEGV, &flb_signal_handler);
+//     signal(SIGFPE,  &flb_signal_handler);
 }
 
 static int custom_set_property(struct flb_custom_instance *in, char *kv)
