2360ab59f38cf9c65c9131c2d36c2ef724972aab
Wesley Pettit <wppttt@amazon.com>
2022-03-07 22:44:21 -0800

    wip

--- plugins/filter_multiline/ml.c
+++ plugins/filter_multiline/ml.c
@@ -578,7 +578,7 @@ static int cb_ml_filter(const void *data, size_t bytes,
         return ml_filter_partial(data, bytes, tag, tag_len,
                                  out_buf, out_bytes,
                                  f_ins, i_ins,
-                                 filter_context, config)
+                                 filter_context, config);
     }
 
     /* 'parser' mode */
