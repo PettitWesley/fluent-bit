720a63fc78ef6a42cae9f328c6511f50e6eb89a9
Wesley Pettit <wppttt@amazon.com>
2022-03-07 23:06:11 -0800

    wip

--- plugins/filter_multiline/ml_concat.h
+++ plugins/filter_multiline/ml_concat.h
@@ -46,7 +46,7 @@ int is_partial(msgpack_object *map);
 int is_partial_last(msgpack_object *map);
 char *get_partial_id(msgpack_object *map);
 struct split_message_packer *get_packer(struct mk_list *packers, const char *tag, 
-                                        char *input_name, char *partial_id)
+                                        char *input_name, char *partial_id);
 struct split_message_packer *create_packer(const char *tag, char *input_name, char *partial_id,
                                            msgpack_object *map, char *multiline_key_content,
                                            struct flb_time *tm);
