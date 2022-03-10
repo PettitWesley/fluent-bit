3a7b15593422326114ee2c110461439d64bbf173
Wesley Pettit <wppttt@amazon.com>
2022-03-09 19:58:36 -0800

    wip

--- plugins/filter_multiline/ml.c
+++ plugins/filter_multiline/ml.c
@@ -557,8 +557,7 @@ pack_non_partial:
     }
     msgpack_unpacked_destroy(&result);
 
-    if (return_records == total_records) {
-        flb_info("NOTOUCH YOU CAT");
+    if (partial_records == 0 )) {
         return FLB_FILTER_NOTOUCH;
     } else if (return_records > 0) {
         *out_buf  = tmp_sbuf.data;
