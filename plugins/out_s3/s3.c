5cc088107ca8d8ecc127f35ea93aba0f4d4ff6a5
Leonardo Alminana <leonardo@calyptia.com>
2023-02-28 11:46:04 +0100

    out_s3: added missing structure instance in s3_retry_warn calls
    Signed-off-by: Leonardo Alminana <leonardo@calyptia.com>

--- plugins/out_s3/s3.c
+++ plugins/out_s3/s3.c
@@ -1110,11 +1110,11 @@ put_object:
         if (chunk) {
             chunk->failures += 1;
             if (chunk->failures > ctx->ins->retry_limit){
-                s3_retry_warn(ctx, tag, chunk->input_name, file_first_log_time, FLB_FALSE);
+                s3_retry_warn(ctx, tag, chunk->input_name, chunk->create_time, FLB_FALSE);
                 return -2;
             }
             else {
-                s3_retry_warn(ctx, tag, chunk->input_name, file_first_log_time, FLB_TRUE);
+                s3_retry_warn(ctx, tag, chunk->input_name, chunk->create_time, FLB_TRUE);
                 return -1;
             }
         }
