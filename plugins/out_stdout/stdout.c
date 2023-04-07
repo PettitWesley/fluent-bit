c41c432ab9a0465cea029d713089c903382d01f7
Wesley Pettit <wppttt@amazon.com>
2023-04-07 12:10:32 -0700

    wip

--- plugins/out_stdout/stdout.c
+++ plugins/out_stdout/stdout.c
@@ -181,7 +181,7 @@ static void cb_stdout_flush(struct flb_event_chunk *event_chunk,
         FLB_OUTPUT_RETURN(FLB_ERROR);
     }
 
-    tmp = flb_sds_printf(&test, "this-is-54-chars-1234567890-abcdefghijklmnopqrstuvwxyz %s", "0123456789");
+    tmp = flb_sds_printf(&test, "this-is-54-chars-1234567890-abcdefghijklmnopqrstuvwxyz %s", "A0123456789");
     flb_info("TEST: %s", tmp);
 
 
