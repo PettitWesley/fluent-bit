3b561ec2bfc90b1e8101240cc9566a36ee108735
Wesley Pettit <wppttt@amazon.com>
2023-04-07 12:12:15 -0700

    wip

--- plugins/out_stdout/stdout.c
+++ plugins/out_stdout/stdout.c
@@ -184,6 +184,42 @@ static void cb_stdout_flush(struct flb_event_chunk *event_chunk,
     tmp = flb_sds_printf(&test, "A0123456789 %s", "this-is-54-chars-1234567890-abcdefghijklmnopqrstuvwxyz");
     flb_info("TEST: %s", tmp);
 
+    flb_sds_t test = flb_sds_create_size(64);
+    if (!test) {
+        flb_errno();
+        FLB_OUTPUT_RETURN(FLB_ERROR);
+    }
+
+    tmp = flb_sds_printf(&test, "0123456789 %s", "this-is-54-chars-1234567890-abcdefghijklmnopqrstuvwxyz");
+    flb_info("TEST: %s", tmp);
+
+    flb_sds_t test = flb_sds_create_size(64);
+    if (!test) {
+        flb_errno();
+        FLB_OUTPUT_RETURN(FLB_ERROR);
+    }
+
+    tmp = flb_sds_printf(&test, "123456789 %s", "this-is-54-chars-1234567890-abcdefghijklmnopqrstuvwxyz");
+    flb_info("TEST: %s", tmp);
+
+    flb_sds_t test = flb_sds_create_size(64);
+    if (!test) {
+        flb_errno();
+        FLB_OUTPUT_RETURN(FLB_ERROR);
+    }
+
+    tmp = flb_sds_printf(&test, "3456789 %s", "this-is-54-chars-1234567890-abcdefghijklmnopqrstuvwxyz");
+    flb_info("TEST: %s", tmp);
+
+    flb_sds_t test = flb_sds_create_size(64);
+    if (!test) {
+        flb_errno();
+        FLB_OUTPUT_RETURN(FLB_ERROR);
+    }
+
+    tmp = flb_sds_printf(&test, "AB0123456789 %s", "this-is-54-chars-1234567890-abcdefghijklmnopqrstuvwxyz");
+    flb_info("TEST: %s", tmp);
+
 
 #ifdef FLB_HAVE_METRICS
     /* Check if the event type is metrics, handle the payload differently */
