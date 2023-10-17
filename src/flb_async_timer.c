c74dc5f8523dd4b0c723f39e9cdc9ffe69caac16
Wesley Pettit <wppttt@amazon.com>
2023-10-17 16:46:13 -0700

    wip

--- src/flb_async_timer.c
+++ src/flb_async_timer.c
@@ -19,6 +19,7 @@
 
 #include <fluent-bit/flb_coro.h>
 #include <fluent-bit/flb_output.h>
+#include <fluent-bit/flb_async_timer.h>
 
 void flb_async_timer_destroy(struct flb_out_async_timer *timer)
 {
