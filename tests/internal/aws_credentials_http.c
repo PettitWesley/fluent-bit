fbf3d8150d30ee858984b4aafa7252610ff15071
Wesley Pettit <wppttt@amazon.com>
2024-04-18 14:50:09 -0700

    tests: internal: define aws http provider test list
    Signed-off-by: Wesley Pettit <wppttt@amazon.com>

--- tests/internal/aws_credentials_http.c
+++ tests/internal/aws_credentials_http.c
@@ -788,9 +788,15 @@ static void test_http_validator_invalid_host()
 }
 
 TEST_LIST = {
-    { "test_http_provider" , test_http_provider},
-    { "test_http_provider_error_case" , test_http_provider_error_case},
-    { "test_http_provider_malformed_response" ,
-    test_http_provider_malformed_response},
+    { "test_http_provider", test_http_provider},
+    { "test_http_provider_error_case", test_http_provider_error_case},
+    { "test_http_provider_malformed_response", test_http_provider_malformed_response},
+    { "test_http_provider", test_http_provider},
+    { "test_http_provider_ecs_case", test_http_provider_ecs_case},
+    { "test_http_provider_eks_with_token", test_http_provider_eks_with_token},
+    { "test_http_provider_eks_with_token_file", test_http_provider_eks_with_token_file},
+    { "test_http_provider_https_endpoint", test_http_provider_https_endpoint},
+    { "test_http_provider_server_failure", test_http_provider_server_failure},
+    { "test_http_validator_invalid_host", test_http_validator_invalid_host},
     { 0 }
 };
